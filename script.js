f919b5fec17becf8d70d836ce5954bdaef46db51
unbearabletruth <antaarty@gmail.com>
2023-05-13 13:38:10 +0700

    reverseString test

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function capitalize(string) {
+function capitalize(string){
     const firstLetter = string.charAt(0).toUpperCase();
     const rest = string.slice(1);
     return firstLetter + rest;
+}
 
-  }
+function reverseString(string){
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
 
-module.exports = capitalize;
+module.exports = {capitalize, reverseString};
--- script.test.js
+++ script.test.js
@@ -1,5 +1,10 @@
-const capitalize = require('./script');
+const {capitalize, reverseString} = require('./script');
 
 test("string is capitalized", () => {
   expect(capitalize("hey")).toBe("Hey");
 });
+
+test("reversed string", () => {
+  expect(reverseString("hey")).toBe("yeh");
+  expect(reverseString("unbearable")).toBe("elbaraebnu");
+})
