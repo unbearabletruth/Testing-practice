19ce99bf9ff99087b8521041c4a750f6efa64320
unbearabletruth <antaarty@gmail.com>
2023-05-14 11:46:10 +0700

    Caesar cipher + analyse array

--- script.js
+++ script.js
@@ -35,4 +35,69 @@ class Calculator {
     }
 }
 
-module.exports = {capitalize, reverseString, Calculator};
+function caesarCipher(string, shift){
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+        let charCode = string[i].charCodeAt() + shift;
+        if (string[i].charCodeAt() < 97 || string[i].charCodeAt() > 122){
+            newString += string[i];
+        }
+        else if (charCode > 122){
+            let dif = charCode - 122;
+            charCode = 96 + dif;
+            newString += String.fromCharCode(charCode);
+        } else {
+            newString += String.fromCharCode(charCode);
+        }
+        
+    }
+    return newString;
+}
+
+function analyseArray(array){  
+    
+    const average = () => {
+        let avg;
+        let sum = 0;
+        for (let number of array){
+        sum += number;
+    }
+    avg = sum / array.length;
+    return avg;
+    }
+
+    const min = () => {
+        let min = array[0];
+        for (let number of array){
+            if (number < min){
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    const max = () => {
+        let max = 0;
+        for (let number of array){
+            if (number > max){
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    const length = () => {
+        return array.length;
+    }
+
+    let object = {
+        average: average(),
+        min: min(),
+        max: max(),
+        length: length()
+    };  
+    
+    return object;  
+}
+
+module.exports = {capitalize, reverseString, Calculator, caesarCipher, analyseArray};
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const {capitalize, reverseString, Calculator} = require('./script');
+const {capitalize, reverseString, Calculator, caesarCipher, analyseArray} = require('./script');
 
 test("string is capitalized", () => {
   expect(capitalize("hey")).toBe("Hey");
@@ -7,11 +7,22 @@ test("string is capitalized", () => {
 test("reversed string", () => {
   expect(reverseString("hey")).toBe("yeh");
   expect(reverseString("unbearable")).toBe("elbaraebnu");
-})
+});
 
 test("calculated", () => {
   expect(new Calculator(2, 5).add()).toBe(7);
   expect(new Calculator(10, 3).substract()).toBe(7);
   expect(new Calculator(4, 9).multiply()).toBe(36);
   expect(new Calculator(8, 2).divide()).toBe(4);
+});
+
+test("into Caesar cipher", () => {
+  expect(caesarCipher("sun", 1)).toBe("tvo");
+  expect(caesarCipher("zoo", 2)).toBe("bqq");
+  expect(caesarCipher("chimp", 0)).toBe("chimp");
+  expect(caesarCipher("hey, there", 1)).toBe("ifz, uifsf");
+});
+
+test("analysed array", () => {
+  expect(analyseArray([5, 3, 4, 6])).toEqual({average: 4.5, min: 3, max: 6, length: 4});
 })
